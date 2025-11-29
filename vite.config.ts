a2013b17b624df3309f529522a8640e8e21ed424
Rich Bisso <rich.bisso@gmail.com>
2025-11-29 08:03:25 -0800

    Configure React plugin to process .js files as JSX

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,9 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
+    react({
+      include: ['**/*.jsx', '**/*.tsx', '**/*.js'],
+    }),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
